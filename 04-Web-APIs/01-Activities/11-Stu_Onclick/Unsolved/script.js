97768f48f22ad04d1894b3107bede143d901457d
Julie Schaub <julie.schaub@hotmail.com>
2020-07-02 16:39:35 -0500

    updated files

---
+++ 04-Web-APIs/01-Activities/11-Stu_Onclick/Unsolved/script.js
@@ -0,0 +1,10 @@
+var count = 0;
+var increment = document.querySelector("#increment");
+var decrement = document.querySelector("#decrement");
+var countEl = document.querySelector("#count");
+
+funtion setCounterText () {
+countEl.textContent = count
+}
+
+incrementEl.addEventListener.
---
+++ 04-Web-APIs/01-Activities/13-Stu_Preventing_Default_Events/script.js
@@ -0,0 +1,8 @@
+var billAmount = document.querySelector(#billAmount);
+var tipAmount = document.querySelector(#tipAmount);
+var newTotal = document,querySelector(#newTotal);
+
+newTotal = ((billAmount) + (billAmount * tipAmount));
+
+
+return newTotal
